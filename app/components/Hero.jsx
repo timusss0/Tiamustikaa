e9725de46dc7e7b144402581684de6d44e43f23e
timusss0 <mustikavia86@gmail.com>
2025-11-18 01:40:27 +0700

    fixing hero section again

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -28,7 +28,7 @@ const item = {
     variants={container}
     initial="hidden"
     animate="show"
-    id="/" className="max-w-2xl md:mb-88 m-auto flex flex-col md:items-center md:justify-center lg:justify-center text-center lg:items-center pb-12 md:22 overflow-hidden">  
+    id="/" className="max-w-2xl mb-30 md:mb-88 m-auto flex flex-col md:items-center md:justify-center lg:justify-center text-center lg:items-center pb-12 md:22 overflow-hidden">  
         <CursorProvider>
         <Cursor />
         <CursorFollow>Tia</CursorFollow>
@@ -39,7 +39,7 @@ const item = {
             <motion.h1 variants={item} className="text-xl md:text-3xl lg:text-5xl font-semibold md:mt-3 leading-tight">Fullstack Developer</motion.h1>
         </motion.div>
         {/* button */}
-        <motion.div variants={item} className="flex flex-col md:flex-row items-center gap-8 my-8  ">
+        <motion.div variants={item} className="flex flex-col md:flex-row items-center gap-4 my-4  ">
           <Link href="#portfolio" className="w-full text-sm lg:text-lg md:text-xl bg-gradient-to-r from-pink-300 to-blue-300  md:w-auto py-3 md:py-3 px-8 text-black rounded-full font-semibold flex items-center justify-center gap-2 hover:opacity-90 transition-opacity duration-300">My Portfolio     <HiOutlineViewGrid /></Link>
           <Link href="/resume.pdf" className="w-full md:w-auto text-sm lg:text-lg md:text-xl bg-gradient-to-r from-pink-100 to-blue-100 border-gray-300 border-2 py-2.5 px-8 text-gray-800 rounded-full font-medium flex items-center justify-center gap-2 hover:opacity-90 transition-all duration-300 shadow-sm" download>My Resume  <HiDownload /></Link>
         </motion.div>
